21bf975ee1dc2638b6e7a74b8d5cbdd411e7a434
VQ253TV <maciej.zacharski@gds.ey.com>
2025-04-17 10:27:41 +0200

    dialogbox tests

--- PageObjects/dialogPage.ts
+++ PageObjects/dialogPage.ts
@@ -14,25 +14,49 @@ export class DialogPage extends HelperBase {
 
         const openDialog = this.page.locator('nb-card', {hasText: 'Open Dialog'})     
 
-        await openDialog.getByRole('button', {name: 'Open Dialog with component'}).click()
+        await openDialog.getByRole('button', {name: 'Open Dialog with component'}).click[0]
 
        
     }
+
+    async opendialogboxWithBackdrop() {
+
+        const openDialog = this.page.locator('nb-card').filter({ hasText: 'Open Without Backdrop' });
+          
+        await openDialog.getByRole('button', {name: 'Open Dialog with backdrop'}).click()
+       
+    }
+
+    async opendialogboxWithBackdropClick() {
+
+        const openDialog = this.page.locator('nb-card', {hasText: 'Open Without Backdrop Click'})     
+
+        await openDialog.getByRole('button', {name: 'Open Dialog with backdrop click'}).click()
+       
+    }
+
+    async opendialogboxWithoutEscClose() {
+
+        const openDialog = this.page.locator('nb-card', {hasText: 'Open Without Esc Close'})     
+
+        await openDialog.getByRole('button', {name: 'Open Dialog with esc close'}).click()
+       
+    }
     
-    async verifyTitleOfDialogboxWithComponent() {
+    async verifyTitleOfDialogbox() {
         
         const getHeader =  await this.page.locator('nb-card-header', {hasText: 'This is a title passed to the dialog component'}).textContent()
         expect(getHeader).toEqual('This is a title passed to the dialog component')
     }
 
-    async verifyDescriptionOfDialogboxWithComponent() {
+    async verifyDescriptionOfDialogbox() {
 
         const getDescription = await this.page.locator('nb-card-body', {hasText: 'Lorem ipsum dolor'}).textContent()
 
         expect(getDescription).toContain('Suspendisse potenti')
     }
 
-    async closeDialogboxWithComponent() {              
+    async closeDialogbox() {              
 
         await this.page.getByRole('button', {name: 'Dismiss Dialog'}).click()
 
